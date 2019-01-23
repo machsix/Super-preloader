1221dc5757ed64ee5b743a0efe4577003e3f3ea9
suchunchen <87444109@qq.com>
2019-01-23 10:54:41 +0800

    Discuz X2.5兼容 https

--- Super_preloader/Super_preloaderPlus_one_New.user.js
+++ Super_preloader/Super_preloaderPlus_one_New.user.js
@@ -1597,7 +1597,7 @@
     },
     {
       name: 'Discuz X2.5修复',
-      url: /^http?:\/\/(bbs.gfan|bbs.xda|bbs.weiphone|bbs.feng|www.weiqitv|www.diypda|f.ppxclub|bbs.sd001|bbs.itiankong)\.(com|cn)/i,
+      url: /^https?:\/\/(bbs.gfan|bbs.xda|bbs.weiphone|bbs.feng|www.weiqitv|www.diypda|f.ppxclub|bbs.sd001|bbs.itiankong)\.(com|cn)/i,
       nextLink: 'auto;',
       autopager: {
         pageElement: '//div[@id="threadlist"] | //div[@id="postlist"]',
