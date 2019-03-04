7f5d5b50ac36cc7e978935bb2a42226182f04849
YFdyh000 <yfdyh000@gmail.com>
2019-03-05 04:57:42 +0800

    Simplified code

--- Super_preloaderPlus_one_ES8.user.js
+++ Super_preloaderPlus_one_ES8.user.js
@@ -6879,7 +6879,6 @@
 
         const findCurSiteInfo = function () {
           const SIIAD = SITEINFO_D.autopager;
-          var Rurl;
           const ii = SITEINFO.length;
 
           if (isChineseUI()) {
@@ -6892,7 +6891,7 @@
 
           for (var i = 0; i < ii; i++) {
             const SII = SITEINFO[i];
-            Rurl = toRE(SII.url);
+            const Rurl = toRE(SII.url);
             if (Rurl.test(url)) {
               if (isChineseUI()) {
                   debug('找到当前站点规则:', SII);
