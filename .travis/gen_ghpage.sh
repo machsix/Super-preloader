9fa5eab280185d42c913f0afd6cf7875212ff689
machsix <28209092+machsix@users.noreply.github.com>
2019-03-30 02:49:37 -0700

    Hotfix: wrong syntax in mydata.json

--- .travis/gen_ghpage.sh
+++ .travis/gen_ghpage.sh
@@ -7,6 +7,8 @@ if [[ -n `git diff --name-status gh-pages..master -- mydata.json` ]]; then
     sed -i 's|_downloads/\w\+/||g' doc/build/super_preloader.html
     curl http://wedata.net/databases/AutoPagerize/items_all.json -X GET -H 'Content-Type: application/json'  -o doc/build/wedata.json
     cp mydata.json doc/build/mydata.json
+    # remove space in mydata.json
+    awk -i -v ORS= -v OFS= '{$1=$1}1' doc/build/mydata.json
     export DAY=`date --date='now' +'%F %X %Z'`
     travis-sphinx deploy -m "${TRAVIS_COMMIT}-${DAY}"
     echo "mydata.json is deployed"
