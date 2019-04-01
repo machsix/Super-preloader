2e7eb9cc27427270b5af5a40cbb2b507f0e2f246
machsix <28209092+machsix@users.noreply.github.com>
2019-03-31 18:03:05 -0700

    Replace execSync by spawnSync

--- .travis/git-log.js
+++ .travis/git-log.js
@@ -1,54 +1,66 @@
-const { execSync } = require('child_process');
+const {spawnSync} = require("child_process");
+const path = require("path");
 
-function gitLog(cwd, obj, magicKey) {
-    magicKey = '^^'||magicKey;
-    cwd = '.'||cwd;
+const gitLog = function(cwd, obj, magicKey) {
+  magicKey = "^^" || magicKey;
+  cwd = cwd || ".";
+  const args = ["log"];
 
-    let logFormat = {
-        commit: "%H",
-        abbreviated_commit: "%h",
-        date: "%cD",
-        committer: "%cn",
-        author_date: "%aD",
-        author: "%an"
-    };
-    let nCommit = "";
-    let fileList = "";
+  let logFormat = {
+    commit: "%H",
+    abbreviated_commit: "%h",
+    date: "%cD",
+    committer: "%cn",
+    author_date: "%aD",
+    author: "%an"
+  };
 
-    if (obj) {
-        if (obj.hasOwnProperty('logFormat')) {
-            logFormat = obj.logFormat;
-        }
-        if (obj.hasOwnProperty('nCommit')) {
-            nCommit = `-n ${obj.nCommit}`;
-        }
-        if (obj.hasOwnProperty('fileList')) {
-            const fileList = " " + obj.fileList.join(" ");
-        }
+  if (obj) {
+    if (obj.hasOwnProperty("nCommit")) {
+      args.push(`-${obj.nCommit}`);
     }
 
-    let strLogFormat = '{%n';
-    for (const prop in logFormat) {
-        strLogFormat += `  \\"${prop}\\": \\"${logFormat[prop]}\\",%n`;
+    if (obj.hasOwnProperty("logFormat")) {
+      logFormat = obj.logFormat;
     }
-    strLogFormat = strLogFormat.slice(0, strLogFormat.length-3) + `%n}${magicKey}`;
+  }
+  let strLogFormat = "{%n";
+  for (const prop in logFormat) {
+    if (typeof logFormat[prop] === "string") {
+      strLogFormat += `  "${prop}": "${logFormat[prop]}",%n`;
+    }
+  }
+  strLogFormat = strLogFormat.slice(0, strLogFormat.length - 3) + `%n}${magicKey}`;
 
-    const cmd = `git log ${nCommit} --pretty=format:"${strLogFormat}"${fileList}`
-    let stdout = execSync(cmd, {cwd: cwd, encoding: "utf8"});
-    commitInfoArray = stdout.split(magicKey).filter((val) => val.length > 0);
+  args.push(`--pretty=format:${strLogFormat}`);
 
-    let commitInfo = '[\n';
-    commitInfoArray.forEach((val,i) => {
-        if (i < commitInfoArray.length-1) {
-            commitInfo += `${val},`;
-        } else {
-            commitInfo += `${val}\n]`;
-        }
-    })
+  args.push("HEAD");
+  if (obj) {
+    if (obj.hasOwnProperty("fileList")) {
+      args.push("--");
+      obj.fileList.forEach((x) => {
+        args.push(x);
+      });
+    }
+  }
 
-    return JSON.parse(commitInfo);
-}
+  const out = spawnSync("git", args, {
+    cwd: path.resolve(process.cwd(), cwd),
+    encoding: "utf8"
+  });
+  const commitInfoArray = out.stdout.split(magicKey).filter((val) => val.length > 0);
+
+  let commitInfo = "[\n";
+  commitInfoArray.forEach((val, i) => {
+    if (i < commitInfoArray.length - 1) {
+      commitInfo += `${val},`;
+    } else {
+      commitInfo += `${val}\n]`;
+    }
+  });
+  return JSON.parse(commitInfo);
+};
 
 module.exports = {
-    gitLog: gitLog
-}
+  gitLog: gitLog
+};
--- .travis/write_mydata.js
+++ .travis/write_mydata.js
@@ -1,27 +1,35 @@
-const fs = require('fs');
-const { gitLog } = require('./git-log');
+/* eslint-disable no-console */
+const fs = require("fs");
+const {gitLog} = require("./git-log");
 
 // strip whitespace in mydata.json
-fs.readFile("../mydata.json", {encoding="utf8"}, (err, data) => {
+fs.readFile("../mydata.json", {encoding: "utf8"}, (err, data) => {
+  if (!err) {
     const mydata = JSON.parse(data);
-    fs.writeFile("../mydata.json", JSON.stringify(mydata), "utf8", ()=>{console.log("mydata.json is re-writted")});
+    fs.writeFile("../mydata.json", JSON.stringify(mydata), "utf8", () => {
+      console.log("mydata.json is re-writted");
+    });
+  }
 });
 
 // get latest update time
-const commitInfo = gitLog('../', {fileList: ['mydata.json'], nCommit: 1});
+const commitInfo = gitLog("..", {
+  fileList: ["README.md"],
+  nCommit: 1
+});
 const lastUpdateDate = new Date(commitInfo[0].date);
 
 const info = {
-    created_by: "mach6",
-    name: "mydata.json",
-    updated_at: lastUpdateDate.toJSON(),
-    required_keys: "url nextLink pageElement",
-    created_at: "2017-09-27T00:00:00.000Z",
-    resource_url: "https://machsix.github.io/Super-preloader/mydata.json",
-    description: "Rule for Super_preloaderPlus_one_New",
-    permit_other_keys: true
+  created_by: "mach6",
+  name: "mydata.json",
+  updated_at: lastUpdateDate.toJSON(),
+  required_keys: "url nextLink pageElement",
+  created_at: "2017-09-27T00:00:00.000Z",
+  resource_url: "https://machsix.github.io/Super-preloader/mydata.json",
+  description: "Rule for Super_preloaderPlus_one_New",
+  permit_other_keys: true
 };
 
-fs.writeFileSync("../mydata_detail.json", JSON.stringify(info), "utf8");
-
-
+fs.writeFile("../mydata_detail.json", JSON.stringify(info), "utf8", () => {
+  console.log("mydata_detail.json is written");
+});
