c2ccda9d541e2a81abbbc02a7fd791b09b3e4a1c
machsix <28209092+machsix@users.noreply.github.com>
2019-04-02 03:25:52 -0700

    v6.6.33 Fix WordPress post

--- .travis/gen_ghpage.sh
+++ .travis/gen_ghpage.sh
@@ -4,8 +4,15 @@ REPO_DIR=`pwd`
 cd ${REPO_DIR}/.travis
 node write_mydata.js
 echo -e "\e[1m\e[41m\e[97mFinish writing mydata_detail.json\e[0m"
+if [ -f "../mydata_dev.json" ]; then
+    node write_mydata.js mydata_dev.json
+    echo -e "\e[1m\e[41m\e[97mFinish writing details for mydata_dev.json\e[0m"
+fi
 cd ${REPO_DIR}
 git add mydata.json
+if [ -f "mydata_dev.json" ]; then
+    git add mydata_dev.json
+fi
 git commit -m "Temporary commit"
 
 git fetch origin gh-pages:gh-pages
@@ -24,6 +31,11 @@ if [[ -n `git diff --name-status gh-pages..master -- mydata.json` ]] || [[ ${TRA
     cp ${REPO_DIR}/mydata.json        ${REPO_DIR}/doc/build/mydata.json
     cp ${REPO_DIR}/mydata_detail.json ${REPO_DIR}/doc/build/mydata_detail.json
     echo -e "\e[1m\e[41m\e[97mMydata is deployed\e[0m"
+    if [ -f ${REPO_DIR}/mydata_dev.json ]; then
+        cp ${REPO_DIR}/mydata_dev.json        ${REPO_DIR}/doc/build/mydata_dev.json
+        cp ${REPO_DIR}/mydata_dev_detail.json ${REPO_DIR}/doc/build/mydata_dev_detail.json
+        echo -e "\e[1m\e[41m\e[97mMydata_dev is deployed\e[0m"
+    fi
 
     export DAY=`date --date='now' +'%F %X %Z'`
     travis-sphinx deploy -m "${TRAVIS_COMMIT}-${DAY}"
