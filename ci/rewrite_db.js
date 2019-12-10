0e57a29d9c52f28bf20e3f8d6aea6dce7c7c7e48
machsix <28209092+machsix@users.noreply.github.com>
2019-12-10 02:13:00 -0500

    docs: new build [UPDATE]

--- ci/rewrite_db.js
+++ ci/rewrite_db.js
@@ -1,32 +1,36 @@
-const logger = require("loglevel");
 const fs = require("fs");
 const {gitLog} = require("./git-log");
 const path = require("path");
-logger.setLevel("info");
+const argv = require("yargs").argv;
 
-const jsonFilePath = process.argv.length >= 2 ? process.argv[2] : path.resolve("../dist/mydata.json");
-// const url = process.argv.length >= 3 ? process.argv[3] : undefined;
-
+// 1. Filename
+const jsonFilePath = argv._.length ? argv._[0] : path.resolve("../dist/mydata.json");
 const jsonFile = path.basename(jsonFilePath);
-const jsonFileName = jsonFile.split(".")[0];
+const jsonFileName = path.basename(jsonFile, path.extname(jsonFile));
+// 2. Mandatory update
+const mandatoryUpdate = argv.update || false;
 
 // strip whitespace in the file
 fs.readFile(jsonFilePath, {encoding: "utf8"}, (err, data) => {
   if (!err) {
     const mydata = JSON.parse(data);
-    fs.writeFile(jsonFilePath, JSON.stringify(mydata), "utf8", () => {
-      logger.info(`${jsonFilePath} is re-written`);
-    });
+    fs.writeFile(jsonFilePath, JSON.stringify(mydata), "utf8", () => {});
+  } else {
+    throw err;
   }
 });
 
+console.log(`${jsonFilePath} is re-written`);
 // get latest update time
 const commitInfo = gitLog({
   fileList: [jsonFilePath],
   nCommit: 1
 });
-logger.debug(commitInfo);
-const lastUpdateDate = new Date(commitInfo[0].date);
+console.log(commitInfo);
+let lastUpdateDate = new Date(commitInfo[0].date);
+if (mandatoryUpdate) {
+  lastUpdateDate = new Date();
+}
 
 const info = {
   created_by: "mach6",
@@ -41,7 +45,7 @@ const info = {
 };
 
 const detailFilePath = path.resolve(path.dirname(jsonFilePath), `${jsonFileName}_detail.json`);
-logger.info(JSON.stringify(info));
+console.log(JSON.stringify(info));
 fs.writeFile(detailFilePath, JSON.stringify(info), "utf8", () => {
-  logger.info(`${detailFilePath} is written`);
+  console.log(`${detailFilePath} is written`);
 });
