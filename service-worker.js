e7b14ee49b1bc217c97a63832973a75b3afd52f3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-08 22:01:23 +0000

    Deploy machsix/Super-preloader to github.com/machsix/Super-preloader.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -34,16 +34,16 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.71962aec.js",
-    "revision": "23d7bf7d0c3d15c8a8e90e1b3dbd43e8"
+    "url": "assets/js/10.13d6ee8a.js",
+    "revision": "8a179f474e2047bfcb8adb4ef1af7fe2"
   },
   {
-    "url": "assets/js/11.6003b924.js",
-    "revision": "df8024d07b8bb76ef80f33a9e69b5a21"
+    "url": "assets/js/11.a41f57ae.js",
+    "revision": "a10d7147e5df38525a437865e811e120"
   },
   {
-    "url": "assets/js/12.b800f92f.js",
-    "revision": "92ff300e187baf925f94672cbdbd5182"
+    "url": "assets/js/12.8f0ca9de.js",
+    "revision": "91a05baaca6f5c7c00b4e2cc54ab2a77"
   },
   {
     "url": "assets/js/13.07f285b3.js",
@@ -58,12 +58,12 @@ self.__precacheManifest = [
     "revision": "33a4e9202fb6a54daa2bfb646bba561f"
   },
   {
-    "url": "assets/js/16.dcf9a5ba.js",
-    "revision": "c627162b8adde6c61f57a6ae0f9fa881"
+    "url": "assets/js/16.939693c9.js",
+    "revision": "d79a57015a67aa912f9d407101745e67"
   },
   {
-    "url": "assets/js/17.eb6b11ca.js",
-    "revision": "a92c2ac6fd3751d4f2bce06d149ebdfd"
+    "url": "assets/js/17.5904f6ed.js",
+    "revision": "7b4569e4c1a2545c1db9204bb2575c93"
   },
   {
     "url": "assets/js/18.fd8100b4.js",
@@ -102,36 +102,36 @@ self.__precacheManifest = [
     "revision": "da197c6c55ac469f7c9006b53c9036b8"
   },
   {
-    "url": "assets/js/7.27232d1d.js",
-    "revision": "3b175dcfb5908f9401b1821d43181cc9"
+    "url": "assets/js/7.16a06d49.js",
+    "revision": "c09e04b5082c191f0fd5ae3f9aa528dc"
   },
   {
     "url": "assets/js/8.5af6853c.js",
     "revision": "0761356346897c67a52ac2dd8f037dd3"
   },
   {
-    "url": "assets/js/9.a75167b7.js",
-    "revision": "889c4fa2655d5f62397d269b8101b938"
+    "url": "assets/js/9.da85d86e.js",
+    "revision": "f871801007d9d670686d1e901c96bc86"
   },
   {
-    "url": "assets/js/app.305a6e43.js",
-    "revision": "da6547fd508dbbb80202148ff244fbb5"
+    "url": "assets/js/app.37ba0796.js",
+    "revision": "07a158f6ea00b6bf8506001f9ebe917d"
   },
   {
     "url": "compatibility.html",
-    "revision": "53ccdd95c98467f94e1f510a8b642a37"
+    "revision": "a09731bbc2a1909bf66be8ff50f4ca5c"
   },
   {
     "url": "database.html",
-    "revision": "abb7fe19bc2b2b5863e9296e383dd121"
+    "revision": "1f10c1441c9aa26244d13d6241cfd5e9"
   },
   {
     "url": "example.html",
-    "revision": "26aad0b7ac24c9be19f19e64544683e2"
+    "revision": "56fd97cfe2da2e4d19dd2d7df5a15f53"
   },
   {
     "url": "faq.html",
-    "revision": "50e7b517773225327b006a2acdbfcfdd"
+    "revision": "73acfccedae6c9dcdde00a74564058f8"
   },
   {
     "url": "image/example/baidu.png",
@@ -151,7 +151,7 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "ebf3ab77a3d18fa1a4a2dd9f17948bc3"
+    "revision": "3e3501524b49dd76c79885fd7ea5925b"
   },
   {
     "url": "logo.png",
@@ -159,35 +159,35 @@ self.__precacheManifest = [
   },
   {
     "url": "siterule.html",
-    "revision": "48813dbc987df93992d1362c5d86d962"
+    "revision": "f716a6749ca8195a8232f95fa107f553"
   },
   {
     "url": "zh-cn/compatibility.html",
-    "revision": "7b294ff00952dc1ebc2da3c1db15b311"
+    "revision": "adef2e6efa2ea44536b09856cc355335"
   },
   {
     "url": "zh-cn/database.html",
-    "revision": "3a6d44cdff7e7dbe80eadd588e4e4706"
+    "revision": "8e7ccac8c4cf6c920b6fdde8e76356f0"
   },
   {
     "url": "zh-cn/develop.html",
-    "revision": "b546e49169eb7be3499ebc36e4bc4f2b"
+    "revision": "4dad5ebccd4dca341565e1c2f99180dd"
   },
   {
     "url": "zh-cn/example.html",
-    "revision": "5ca7bad257113f780153ddfe76828579"
+    "revision": "251d5fdaed2b7b8d43ff58b3b259ffe2"
   },
   {
     "url": "zh-cn/faq.html",
-    "revision": "47bbd5e40b808781f0d72b3802a3b8c4"
+    "revision": "2a07a4650641139d5d18d53aed9988ca"
   },
   {
     "url": "zh-cn/index.html",
-    "revision": "13c7aeb56996bb7c473765aac4c68d29"
+    "revision": "93960e077c98d1d0b7ac8465e37f5f21"
   },
   {
     "url": "zh-cn/siterule.html",
-    "revision": "a1ebb6dd9686ca68bd6c4f1d4220eb3a"
+    "revision": "191093ed892d42062b3648f41c5d47b2"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
